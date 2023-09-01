9078cc54be6e9bc355cb9957579cd704cdda89aa
Levan Elizbarashvili <leveliz90@gmail.com>
2023-09-01 18:32:02 +0400

    fixed background

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import "./App.css";
 import Question from "./components/Question";
 import Startscreen from "./components/Startscreen";
-import Blob2 from "./assets/Blob2";
-import Blob1 from "./assets/Blob1";
 import { useState } from "react";
 
 function App() {
@@ -14,9 +12,7 @@ function App() {
 
   return (
     <div className="main-div">
-      <Blob1 />
       {started ? <Question /> : <Startscreen toggleStart={toggleStart} />}
-      <Blob2 />
     </div>
   );
 }
